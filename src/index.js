05c0bb286597f6094f550060aa53f27994c59d90
Dhaval Jivarajani <dhaval.jivarajani@debeersgroup.com>
2026-01-20 15:12:10 +0530

    Fix blank pages: Update homepage and router basename to /kiosk

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.NODE_ENV === 'production' ? window.location.pathname.split('/')[1] : "/"}>
+  <BrowserRouter basename="/kiosk">
     <App />
   </BrowserRouter>
 );
