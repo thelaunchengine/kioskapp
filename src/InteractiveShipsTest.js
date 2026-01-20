5839b483205635d535ee488cfb8e04eafb7cc0ff
Dhaval Jivarajani <dhaval.jivarajani@debeersgroup.com>
2026-01-20 20:02:12 +0530

    Switch map tiles to Esri World Imagery (Satellite)

--- src/InteractiveShipsTest.js
+++ src/InteractiveShipsTest.js
@@ -266,8 +266,8 @@ function InteractiveShipsTest() {
                     style={{ height: '100%', width: '100%' }}
                 >
                     <TileLayer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                        attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
                     />
                     {isMiami && rightWhaleData && <GeoJSON data={rightWhaleData} style={{ color: 'red', fillColor: 'red', fillOpacity: 0.2 }} />}
                     {isMiami && humpbackWhaleData && <GeoJSON data={humpbackWhaleData} style={{ color: 'blue', fillColor: 'blue', fillOpacity: 0.2 }} />}
